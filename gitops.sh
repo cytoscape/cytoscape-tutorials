4124389587fafc90b8bda202274d07b1300a9d66
Alexander Pico <alex.pico@gladstone.ucsf.edu>
2018-09-05 10:53:04 -0700

    Update and rename gitops to gitops.sh

--- gitops.sh
+++ gitops.sh
@@ -4,8 +4,8 @@
 # ssh access to 132.239.8.177 requires an initial login to grenache.ucsd.edu
 # /etc/apache2/sites-available/tutorials.conf contains the following redirect:
 
-<VirtualHost *:80>
-	ServerName tutorials.cytoscape.org
-	Redirect / https://github.com/cytoscape/cytoscape-tutorials/wiki
-</VirtualHost>
+# <VirtualHost *:80>
+# 	ServerName tutorials.cytoscape.org
+#	Redirect / https://github.com/cytoscape/cytoscape-tutorials/wiki
+# </VirtualHost>
 
